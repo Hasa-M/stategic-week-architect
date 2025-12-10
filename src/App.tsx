b2864f79b36f6b44299c6e3c76f2a90a88a64eee
SalvatoreFadda <37809998+SalvatoreFadda@users.noreply.github.com>
2025-12-10 13:38:13 +0100

    App scheleton and global - styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 function App() {
     return (
-        <>
-            <main className="my-5 ml-6 mr-3 rounded-3xl bg-gray-200">
-                <header>
+        <div className="flex flex-row h-full">
+            <main className="my-5 ml-6 mr-3 flex-auto">
+                <header className="pt-5 pb-0 px-0">
                     <span>Title + Notes Control</span>
                 </header>
-                <div>
+                <div className="pt-6 pb-0 px-0">
                     <span>Templates Bar</span>
                 </div>
-                <div>
+                <div className="pt-6 pb-0 px-0">
                     <span>Grid Toolbar</span>
                 </div>
-                <section>
+                <section className="pt-3 pb-0 px-0">
                     <span>Grid</span>
                 </section>
             </main>
-            <aside className="my-5 ml-3 mr-6 rounded-3xl bg-gray-200">Sidebar</aside>
-        </>
+            <aside className="my-5 ml-3 mr-6 md:w-80 2xl:w-120 flex-none">
+                Sidebar
+            </aside>
+        </div>
     );
 }
 
